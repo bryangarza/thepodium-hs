e914539ae070c0bef79cf4c0a4780e0aff385a76
Bryan Garza <brygarza@gmail.com>
2016-03-18 13:58:19 -0700

    Finish adding new fields to Account

--- src/Podium/DB/Table/Account.hs
+++ src/Podium/DB/Table/Account.hs
@@ -12,36 +12,74 @@ import Data.Aeson
 import Data.Default
 import Data.Profunctor.Product.TH (makeAdaptorAndInstance)
 import Data.Text
+import Data.Time (UTCTime)
 import Data.UUID as U
 import Data.UUID.Aeson
 import Opaleye
   ( Column
   , optional
+  , PGArray
+  , PGInt4
   , PGText
+  , PGTimestamptz
   , PGUuid
   , required
   , Table(Table) )
 
-type AccountId = UUID
-type Username  = Text
-type Email     = Text
-type Password  = Text
+type AccountId  = UUID
+type Username   = Text
+type Email      = Text
+type Password   = Text
+type Bio        = Text
+type JoinDate   = UTCTime
+type Location   = Text
+type Profession = Text
+type YearsOfExp = Int
+type Interests  = [Text]
 
-data Account' a b c d = Account
-    { _accountId :: a
-    , _username  :: b
-    , _email     :: c
-    , _password  :: d
+data Account' a b c d e f g h i j = Account
+
+    { _accountId  :: a
+    , _username   :: b
+    , _email      :: c
+    , _password   :: d
+    , _bio        :: e
+    , _joindate   :: f
+    , _location   :: g
+    , _profession :: h
+    , _yearsofexp :: i
+    , _interests  :: j
     } deriving (Eq, Show, Generic)
 
 makeLenses ''Account'
 
-type Account = Account' AccountId Username Email Password
+type Account =
+  Account'
+  AccountId
+  Username
+  Email
+  Password
+  Bio
+  JoinDate
+  Location
+  Profession
+  YearsOfExp
+  Interests
 
 instance ToJSON Account
 
 instance Default Account where
-  def = Account U.nil "foo" "foo@bar.com" "quux123456"
+  def =
+    Account U.nil
+    "foo"
+    "foo@bar.com"
+    "quux123456"
+    "my bio"
+    (read ("2016-03-18 20:21:31.390763 UTC") :: UTCTime)
+    "SF"
+    "Developer"
+    3
+    ["hiking", "sketching", "reading"]
 
 $(makeAdaptorAndInstance "pAccount" ''Account')
 
@@ -49,12 +87,38 @@ $(makeAdaptorAndInstance "pAccount" ''Account')
 -- someTable :: Table <writes>
 --                    <reads>
 
-type ColumnW = Account' (Column PGUuid) (Column PGText) (Column PGText) (Column PGText)
-type ColumnR = Account' (Column PGUuid) (Column PGText) (Column PGText) (Column PGText)
+type ColumnW = Account'
+    (Column PGUuid)
+    (Column PGText)
+    (Column PGText)
+    (Column PGText)
+    (Column PGText)
+    (Column PGTimestamptz)
+    (Column PGText)
+    (Column PGText)
+    (Column PGInt4)
+    (Column (PGArray PGText))
+type ColumnR = Account'
+    (Column PGUuid)
+    (Column PGText)
+    (Column PGText)
+    (Column PGText)
+    (Column PGText)
+    (Column PGTimestamptz)
+    (Column PGText)
+    (Column PGText)
+    (Column PGInt4)
+    (Column (PGArray PGText))
 
 table :: Table ColumnW ColumnR
 table = Table "accountTable" (
-  pAccount Account { _accountId = required "accountId"
-                   , _username  = required "username"
-                   , _email     = required "email"
-                   , _password  = required "password" })
+  pAccount Account { _accountId  = required "accountId"
+                   , _username   = required "username"
+                   , _email      = required "email"
+                   , _password   = required "password"
+                   , _bio        = required "bio"
+                   , _joindate   = required "joindate"
+                   , _location   = required "location"
+                   , _profession = required "profession"
+                   , _yearsofexp = required "yearsofexp"
+                   , _interests  = required "interests"  })
